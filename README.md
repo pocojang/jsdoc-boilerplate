6fc2ec19d1b66b2d04bef4ecf3d4c6c0bbb9efb5
Poco <devjang2016@gmail.com>
2022-08-25 18:58:37 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# jsdoc-boilerplate
+# JSDoc Boilerplate
+
+## Usage
+
+### Install
+
+```bash
+npm install
+or
+yarn
+```
+
+### Build
+
+```bash
+npm run docs
+or
+yarn docs
+```
+
+#### Reference
+
+- [jsdoc.app](https://jsdoc.app)
---
+++ src/index.js
@@ -0,0 +1,51 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function Book(title, author) {}
+
+/**
+ * Both of these will link to the bar function.
+ * @see {@link bar}
+ * @see bar
+ */
+function foo() {}
+
+// Use the inline {@link} tag to include a link within a free-form description.
+/**
+ * @see {@link foo} for further information.
+ * @see {@link http://github.com|GitHub}
+ */
+function bar() {}
+
+/**
+ * Generic dairy product.
+ * @constructor
+ */
+function DairyProduct() {}
+
+/**
+ * Check whether the dairy product is solid at room temperature.
+ * @abstract
+ * @return {boolean}
+ */
+DairyProduct.prototype.isSolid = function () {
+	throw new Error('must be implemented by subclass!');
+};
+
+/**
+ * Cool, refreshing milk.
+ * @constructor
+ * @augments DairyProduct
+ */
+function Milk() {}
+
+/**
+ * Check whether milk is solid at room temperature.
+ * @return {boolean} Always returns false.
+ */
+Milk.prototype.isSolid = function () {
+	return false;
+};
